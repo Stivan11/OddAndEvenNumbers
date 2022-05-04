be4793cb0562ddfbbd18333973732fcc49535d71
Stivan11 <104930798+Stivan11@users.noreply.github.com>
2022-05-04 21:40:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace OddAndEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var rnd = new Random();
+            var queue = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int random = rnd.Next(1, 101);
+                queue.Enqueue(random);
+            }
+
+            var even = new List<int>();
+            var odd = new List<int>();
+
+            while (queue.Any())
+            {
+                int currentNumber = queue.Dequeue();
+
+                if (currentNumber % 2 == 0)
+                {
+                    even.Add(currentNumber);
+                }
+                else
+                {
+                    odd.Add(currentNumber);
+                }
+            }
+
+            var merged = new List<int>();
+
+            if (even.Count >= odd.Count)
+            {
+                for (int i = 0; i < even.Count; i++)
+                {
+                    merged.Add(even[i]);
+                    if (i < odd.Count)
+                    {
+                        merged.Add(odd[i]);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < odd.Count; i++)
+                {
+                    if (i < even.Count)
+                    {
+                        merged.Add(even[i]);
+                    }
+                    merged.Add(odd[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", merged));
+
+        }
+    }
+}
